b4c508b1c0aafbfcaad8b53e55a922a106bbf4e5
R.B. Ahlul <cakahlul@gmail.com>
2017-02-02 19:39:48 +0700

    20170202 | Esasjana | Add new base validator

---
+++ user-security/usm-businesslogic/src/main/java/com/anabatic/usm/businesslogic/validator/BaseValidator.java
@@ -0,0 +1,88 @@
+/**
+ * 
+ */
+package com.anabatic.usm.businesslogic.validator;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.anabatic.usm.core.util.ErrorCode;
+
+/**
+ * @author ahlul.esasjana
+ * from BaseValidator Pocket Core
+ *
+ */
+public abstract class BaseValidator<T> implements IValidator<T> {
+	protected Map<String, ErrorCode> error = new HashMap<String, ErrorCode>();
+	
+	@Override
+	public void validateDelete(T object) {
+		error.clear();
+		validateDelete(object);
+	}
+
+	@Override
+	public void validate(T object) {
+		error.clear();
+		validateObject(object);
+	}
+
+	public boolean hasError() {
+		if(error.isEmpty()) return false;
+		return true;
+	}
+	
+	public Map<String, ErrorCode> getErrors() {
+		return error;
+	}
+	
+	public boolean validateCode(String input){
+		Pattern pattern = Pattern.compile("^[a-zA-Z0-9\\-]+$");
+		Matcher matcher = pattern.matcher(input);
+		return matcher.matches();
+	}
+	
+	public boolean isNumberOnly(String input){
+		Pattern pattern = Pattern.compile("^[0-9]+$");
+		Matcher matcher = pattern.matcher(input);
+		return matcher.matches();
+	}
+	
+	public boolean validatePhoneNumber(String input){
+		//validate phone numbers of format "1234567890"
+		if (input.matches("\\d{10}")) return true;
+		//validating phone number with -, . or spaces
+		else if(input.matches("\\d{3}[-\\.\\s]\\d{3}[-\\.\\s]\\d{4}")) return true;
+		//validating phone number with extension length from 3 to 5
+		else if(input.matches("\\d{3}-\\d{3}-\\d{4}\\s(x|(ext))\\d{3,5}")) return true;
+		//validating phone number where area code is in braces ()
+		else if(input.matches("\\(\\d{3}\\)-\\d{3}-\\d{4}")) return true;
+		//return false if nothing matches the input
+		else return false;
+	}
+	
+	public boolean isAlphabetOnly(String input){
+		Pattern pattern = Pattern.compile("^[a-zA-Z][a-zA-Z ]+$");
+		Matcher matcher = pattern.matcher(input);
+		return matcher.matches();
+	}
+	
+	public boolean isAlphanumeric(String input){
+		Pattern pattern = Pattern.compile("^[a-zA-Z0-9 ]+$");
+		Matcher matcher = pattern.matcher(input);
+		return matcher.matches();
+	}
+	
+	public boolean validateRegex(String input, String regex){
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(input);
+		return matcher.matches();
+	}
+	
+	protected abstract void validateObject(T object);
+	protected abstract void validateDel(T object);
+	
+}
