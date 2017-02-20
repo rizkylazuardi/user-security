4cbfaa7fe98f134244513d4ca420746b0bdd73af
R.B. Ahlul <cakahlul@gmail.com>
2017-02-20 15:31:48 +0700

    Fixing app context

--- user-security/usm-persistence/src/main/java/com/anabatic/usm/persistence/config/ConfigurationDatabase.java
+++ user-security/usm-persistence/src/main/java/com/anabatic/usm/persistence/config/ConfigurationDatabase.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.Scope;
 
 
